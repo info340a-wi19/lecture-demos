c6f8851ffb3ad1acefe70d96d674ede209022c4f
Joel Ross <joelross@uw.edu>
2019-01-28 12:23:06 -0800

    End of lecture code

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,4 +1,24 @@
 'use strict';
 
 //JavaScript goes here!
+console.log("Hello World");
+
+console.log("I'm doing JavaScript!");
+
+let xString = "Hello";
+xString = 42;
+console.log(x);
+
+let animalType = 'dog'
+let favoriteAnimal = "cat"
+
+let dogsObj = {
+    fido: {name: 'fido', breed: 'greyhound'},
+    spot: {name: 'spot', breed: 'mutt'},  
+};
+
+for(let i=0; i<array.length; i++){
+
+}
+
 
