86d98f7731b2f14ef732f8a8e9eb334eeb5846b1
Joel Ross <joelross@uw.edu>
2019-02-13 12:26:44 -0800

    End of es6 demo

--- es6/js/index.js
+++ es6/js/index.js
@@ -3,3 +3,53 @@
 //pre-written messages
 let MESSAGES = ["Hello world!", "Goodbye y'all", "silence", "...", "Bueller?"];
 
+class Card {
+
+    constructor(text) {
+        this.text = text; //assign to field
+        this.clickCount = 0;
+    }
+
+    //return a DOM representation of the card
+    render() {
+
+
+        let cardElement = document.createElement('div');
+        cardElement.classList.add("card");
+        cardElement.textContent = this.text+ " ("+this.clickCount+")";
+
+        cardElement.addEventListener('click', () => {
+            this.clickCount++;
+            console.log("Event handler called on ", this);
+            cardElement.textContent = this.text+ " ("+this.clickCount+")"; //update display
+        });
+
+        return cardElement;
+    }
+
+}
+
+
+let card = new Card("Hello world");
+console.log(card.text);
+
+let cardElement = card.render()
+let content = document.querySelector('#content')
+content.appendChild(cardElement);
+
+content.appendChild((new Card("Snow day!")).render());
+
+let messageCards = MESSAGES.map((msg) => new Card(msg))
+
+messageCards.forEach((card) => content.appendChild(card.render()))
+
+// function foo() {
+//     console.log("I was called on", this);
+// }
+
+// foo();
+
+let foo = () => console.log("foo");
+
+foo()
+
